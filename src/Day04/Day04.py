843b8dc8b0b5b34fcf1b47ebc850c03e3dc9465a
Maciej Socha <maciej.socha@orange.com>
2022-12-06 13:26:12 +0100

    Day04

--- src/Day04/Day04.py
+++ src/Day04/Day04.py
@@ -8,11 +8,11 @@ for row in data.values:
 
     first = [x for x in range(int(row[0].split('-')[0]), int(row[0].split('-')[1])+1)]
     second = [x for x in range(int(row[1].split('-')[0]), int(row[1].split('-')[1])+1)]
-    if all(x in first for x in second):
+    if any(x in first for x in second):
         elem = elem + 1
         print(second)
     else:
-        if all(x in second for x in first):
+        if any(x in second for x in first):
             print(first)
             elem = elem + 1
 
